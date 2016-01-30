ffb329d3447710648c666fe617bd4aa7941471ae
dabeng <zhexiany@gmail.com>
2016-01-30 20:37:37 +0800

    refactor the simple popup with native javascript

--- js/simplepopup.js
+++ js/simplepopup.js
@@ -6,7 +6,7 @@
   window.simplePopup = function(type, message, defaultText) {
     var dtd = $.Deferred();
     var appendSimplePopup = function() {
-      var simpPopup = document.createElement(
+      document.body.insertAdjacentHTML('beforeend',
         '<div id="popup-overlay">' +
           '<div class="simple-popup">' +
             '<span class="popup-icon"></span>' +
@@ -14,140 +14,143 @@
             '<div class="popup-buttons"></div>' +
           '</div>' +
         '</div>');
-      document.body.appendChild(simpPopup);
     };
     var cleanupSimplePopup = function() {
-      $('#popup-overlay').find('.popup-buttons').children().remove()
-        .end().siblings('.popup-input').remove();
-      document.querySelector('#popup-overlay').querySelector('.popup-buttons').removeChild();
-      document.querySelector('#popup-overlay').querySelector('.popup-buttons').removeChild();
+      var overlay = document.querySelector('#popup-overlay');
+      overlay.querySelector('.popup-buttons').removeChild();
+      overlay.querySelector('.popup-input').removeChild();
     };
     var appendPopupInput = function() {
-      $('#popup-overlay').find('.popup-message')
-        .after('<input type="text" class="popup-input"/>');
+      document.querySelector('#popup-overlay').querySelector('.popup-message')
+        .insertAdjacentHTML('afterend', '<input type="text" class="popup-input"/>');
     };
     var appendPopupButtons = function(type) {
-      var popupButtons = $('#popup-overlay').find('.popup-buttons');
+      var popupButtons = document.querySelector('#popup-overlay').querySelector('.popup-buttons');
       if (type === 0) {
-        popupButtons.append('<button type="button" class="btn-ok">OK</button>');
+        popupButtons.insertAdjacentHTML('beforeend', '<button type="button" class="btn-ok">OK</button>'));
       } else {
-        popupButtons.append('<button type="button" class="btn-ok">OK</button>' +
-          '<button type="button" class="btn-cancel">Cancel</button>');
+        popupButtons.insertAdjacentHTML('beforeend', '<button type="button" class="btn-ok">OK</button>' +
+          '<button type="button" class="btn-cancel">Cancel</button>'));
       }
     };
     var appendDisableScrollbar = function() {
-      if ($(document).height() > $(window).height()) {
-        var topOffset = $(document).scrollTop();
-        $('body').addClass('disable-scrollbar').css('top', -topOffset + 'px').data('scrolltop', topOffset);
+      var body = document.body;
+      if (body.scrollHeight > window.screen.availHeight) {
+        var topOffset = body.scrollTop;
+        body.classList.add('disable-scrollbar');
+        body.style.top = -topOffset + 'px';
+        body.dataset.scrolltop = topOffset;
       }
     };
     var recoverOriginalScrollbar = function() {
-      if ($(document).height() > $(window).height()) {
-        $('body').removeClass('disable-scrollbar').css('top','');
-        $(document).scrollTop($('body').data('scrolltop')); // compatible wirting for Chrome and Firefox
+      var body = document.body;
+      if (body.scrollHeight > window.screen.availHeight) {
+        body.classList.remove('disable-scrollbar');
+        body.style.top = '';
+        body.scrollTop = body.dataset.scrolltop;
       }
     };
     var bindButtonHandler = function(type) {
-      var overlay = $('#popup-overlay');
-      var btns = overlay.find('.popup-buttons').children();
+      var overlay = document.querySelector('#popup-overlay');
+      var btns = overlay.querySelector('.popup-buttons').children;
       if (type === 0) {
-        btns.on('click', function() {
+        btns.querySelector('.btn-ok').addEventListener('click', function() {
           recoverOriginalScrollbar();
-          overlay.removeClass('show-popup')
-            .find('.simple-popup').data('dtd').resolve();
-        });
+          overlay.classList.remove('show-popup')
+          overlay.querySelector('.simple-popup').dataset.dtd.resolve();
+        }, false);
       } else if (type === 1) {
-        btns.filter('.btn-ok').on('click', function() {
+        btns.querySelector('.btn-ok').addEventListener('click', function() {
           recoverOriginalScrollbar();
-          overlay.removeClass('show-popup')
-            .find('.simple-popup').data('dtd').resolve(true);
-        })
-        .siblings('.btn-cancel').on('click', function() {
+          overlay.classList.remove('show-popup')
+          overlay.querySelector('.simple-popup').dataset.dtd.resolve(true);
+        }, false);
+        btns.querySelector('.btn-cancel').addEventListener('click', function() {
           recoverOriginalScrollbar();
-          overlay.removeClass('show-popup')
-            .find('.simple-popup').data('dtd').resolve(false);
-        });
+          overlay.classList.remove('show-popup')
+          overlay.querySelector('.simple-popup').dataset.dtd.resolve(false);
+        }, false);
       } else {
-        overlay.find('.popup-input').on('keyup', function(event) {
-          if (event.which === 13) {
+        overlay.querySelector('.popup-input').addEventListener('keyup', function(event) {
+          if (event.keyCode === 13) {
             recoverOriginalScrollbar();
             var value = event.target.value.trim();
-            overlay.removeClass('show-popup')
-              .find('.popup-input').val(defaultText)
-              .parent().data('dtd').resolve(value);
+            overlay.classList.remove('show-popup');
+            overlay.querySelector('.popup-input').value = defaultText;
+            overlay.querySelector('.simple-popup').dataset.dtd.resolve(value);
           }
         });
-        btns.filter('.btn-ok').on('click', function() {
+        btns.querySelector('.btn-ok').addEventListener('click', function() {
           recoverOriginalScrollbar();
-          var value = overlay.find('.popup-input').val().trim();
-          overlay.removeClass('show-popup')
-            .find('.popup-input').val(defaultText)
-            .parent().data('dtd').resolve(value);
-        })
-        .siblings('.btn-cancel').on('click', function() {
+          var value = overlay.querySelector('.popup-input').value.trim();
+          overlay.classList.remove('show-popup');
+          overlay.querySelector('.popup-input').value = defaultText;
+          overlay.querySelector('.simple-popup').dataset.dtd.resolve(value);
+        }, false);
+        btns.querySelector('.btn-cancel').addEventListener('click', function() {
           recoverOriginalScrollbar();
-          overlay.removeClass('show-popup')
-            .find('.popup-input').val(defaultText)
-            .parent().data('dtd').resolve('');
-        });
+          overlay.classList.remove('show-popup');
+          overlay.querySelector('.popup-input').value = defaultText;
+          overlay.querySelector('.simple-popup').dataset.dtd.resolve('');
+        }, false);
       }
     };
 
     // With the help of the following function, we enfore browser to repaint in order to
     // fire up the first time transition of alert icon.
     var startIconTransition = function() {
-      var overlay = $('#popup-overlay')[0];
+      var overlay = document.querySelector('#popup-overlay');
       overlay.style.offsetWidth = overlay.offsetWidth;
       overlay.classList.add('show-popup');
     };
 
     if (type === 0) { // alert box
-      if (!$('.simple-popup').length) {
+      if (!document.querySelector('.simple-popup')) {
         appendSimplePopup();
         appendPopupButtons(0);
         bindButtonHandler(0);
         startIconTransition();
-      } else if (!$('.alert-box').length) {
+      } else if (!document.querySelector('.alert-box')) {
         cleanupSimplePopup();
         appendPopupButtons(0);
         bindButtonHandler(0);
       }
-      $('#popup-overlay').find('.simple-popup').attr('class', 'simple-popup alert-box')
-        .find('.popup-icon').text('!');
+      document.querySelector('#popup-overlay').querySelector('.simple-popup').attr('class', 'simple-popup alert-box')
+      document.querySelector('#popup-overlay').querySelector('.popup-icon').text('!');
     } else if (type === 1) { // confirm box
-      if (!$('.simple-popup').length) {
+      if (!document.querySelector('.simple-popup')) {
         appendSimplePopup();
         appendPopupButtons(1);
         bindButtonHandler(1);
         startIconTransition();
-      } else if (!$('.confirm-box').length) {
+      } else if (!document.querySelector('.confirm-box')) {
         cleanupSimplePopup();
         appendPopupButtons(1);
         bindButtonHandler(1);
       }
-      $('#popup-overlay').find('.simple-popup').attr('class', 'simple-popup confirm-box')
-        .find('.popup-icon').text('?');
+      document.querySelector('#popup-overlay').querySelector('.simple-popup').attr('class', 'simple-popup confirm-box')
+      document.querySelector('#popup-overlay').querySelector('.popup-icon').text('?');
     } else { // prompt box
-      if (!$('.simple-popup').length) {
+      if (!document.querySelector('.simple-popup')) {
         appendSimplePopup();
         appendPopupInput();
         appendPopupButtons(2);
         bindButtonHandler(2);
         startIconTransition();
-      } else if (!$('.prompt-box').length) {
+      } else if (!document.querySelector('.prompt-box')) {
         cleanupSimplePopup();
         appendPopupInput();
         appendPopupButtons(2);
         bindButtonHandler(2);
       }
-      $('#popup-overlay').find('.simple-popup').attr('class', 'simple-popup prompt-box')
-        .find('.popup-icon').text(':)')
-        .siblings('.popup-input').val(defaultText).select();
+      document.querySelector('#popup-overlay').querySelector('.simple-popup').attr('class', 'simple-popup prompt-box')
+      document.querySelector('#popup-overlay').querySelector('.popup-icon').text(':)')
+      document.querySelector('#popup-overlay').querySelector('.popup-input').val(defaultText).select();
     }
     appendDisableScrollbar();
-    $('#popup-overlay').addClass('show-popup')
-      .find('.simple-popup').data('dtd', dtd)
-      .find('.popup-message').text(message);
+    document.querySelector('#popup-overlay').classList.add('show-popup');
+    document.querySelector('#popup-overlay').querySelector('.simple-popup').dataset.dtd = dtd;
+    document.querySelector('#popup-overlay').querySelector('.popup-message').textContent = message;
 
     return dtd;
   }
